fe87c2d67f5744b2a5f28a4b0ccf9cb94c702b8b
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-05-16 02:34:18 +0300

    email authenticate

---
+++ tests/user_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from app.models import User
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='mercy',email = "test@test.com", bio ='default bio',password='123')
+
+   
+ 
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.hashed_password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('mercy'))
