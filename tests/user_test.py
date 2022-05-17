4c0b2714241aa4dd2ade4fed72a3d9eff3ef5e57
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-05-17 10:08:29 +0300

    test writer

--- tests/user_test.py
+++ tests/user_test.py
@@ -5,9 +5,7 @@ from app.models import User
 class UserTest(unittest.TestCase):
     def setUp(self):
         self.new_user = User(username='mercy',email = "test@test.com", bio ='default bio',password='123')
-
-   
- 
+        
     def test_no_access_password(self):
         with self.assertRaises(AttributeError):
             self.new_user.hashed_password
